a10bc0a139ba0692af290a55aa9dc67d0e2a549c
KYHSGeekCode <jourhyang123@gmail.com>
2021-07-14 15:21:53 +0900

    :bug: typo

--- include_html.js
+++ include_html.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function () {
         if (includePath) {
             var w = el.dataset.predW ?? 200;
             var h = el.dataset.predH ?? 100;
-            el.outerHTML = "<div style = \"width:" + predW + "px; height: "+ predH; "px; background: #CCCCCC; \"></div>";
+            el.outerHTML = "<div style = \"width:" + w + "px; height: "+ h; "px; background: #CCCCCC; \"></div>";
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
